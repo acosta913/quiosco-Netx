7ebc60eaae3772943a2fc6bda2e564b5bb4415e4
acosta <acostapaez913@gmail.com>
2025-12-12 00:54:55 -0500

    :construction: list products & search products & pagination products

---
+++ app/admin/products/search/page.tsx
@@ -0,0 +1,40 @@
+import ProductSearchForm from "@/app/components/products/ProductSearchForm";
+import ProductTable from "@/app/components/products/ProductTable";
+import Heading from "@/app/components/ui/Heading";
+import { prisma } from "@/prisma/lib/prisma";
+
+async function searchProducts(searchTerm: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: searchTerm,
+        mode: "insensitive",
+      },
+    },
+    include: {
+      category: true,
+    },
+  });
+  return products;
+}
+
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
+  const products = await searchProducts(searchParams.search);
+  return (
+    <>
+      <Heading>Resultados de busqueda: {searchParams.search}</Heading>
+      <div className=" flex flex-col lg:flex-row lg:justify-end gap-5">
+        <ProductSearchForm />
+      </div>
+      {products.length ? (
+        <ProductTable products={products} />
+      ) : (
+        <p className=" text-center text-lg">No hay resultados</p>
+      )}
+    </>
+  );
+}
